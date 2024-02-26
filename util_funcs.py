7027be7db109bacfc28a7eb4ce945ab70db3a5e6
melmotonteiro <79711994+telmonteiro@users.noreply.github.com>
2024-02-26 16:44:55 +0000

    forgot this file

--- util_funcs.py
+++ util_funcs.py
@@ -1,6 +1,7 @@
 from astroquery.simbad import Simbad
 import numpy as np, pandas as pd, matplotlib.pylab as plt, os
 from astropy.io import fits
+from PyAstronomy import pyasl
 
 def _get_simbad_data(star, alerts=True):
     """Get selected Simbad data for 'star'.
@@ -87,8 +88,11 @@ def plot_RV_indices(star,df,indices,save):
     plt.figure(figsize=(6, (len(indices)+1)*2))
     plt.suptitle(star, fontsize=14)
     plt.subplot(len(indices)+1, 1, 1)
-    plt.errorbar(df.bjd - 2450000, df.rv, df.rv_err, fmt='k.')
+    if "rv_err" not in df.columns: yerr = 0
+    else: yerr = df.rv_err
+    plt.errorbar(df.bjd - 2450000, df.rv, yerr, fmt='k.')
     plt.ylabel("RV [m/s]")
+    print(indices)
     for i, index in enumerate(indices):
         plt.subplot(len(indices)+1, 1, i+2)
         plt.ylabel(index)
@@ -105,7 +109,6 @@ def stats_indice(star,indices,df):
     Return pandas data frame with statistical data on the indice(s) given: max, min, mean, median, std and N (number of spectra)
     """
     df_stats = pd.DataFrame(columns=["star","indice","max","min","mean","median","std","N_spectra"])
-
     if len(indices) == 1:
         row = {"star":star,"indice":indices,
             "max":max(df[indices]),"min":min(df[indices]),
@@ -156,7 +159,10 @@ def create_average_spectrum(folder_path, extension, files):
         return 0,0
 
     #header of first observation. using the first obs as the final header just because it is easier
-    with fits.open(os.path.join(folder_path, files[0])) as hdul: hdr = hdul[0].header
+    with fits.open(os.path.join(folder_path, files[0])) as hdul: 
+        hdr = hdul[0].header
+        date_bjd = np.around(hdr["HIERARCH ESO DRS BJD"])
+        hdr["HIERARCH ESO DRS BJD"] == date_bjd
 
     #load each data and pad or truncate to a common length
     data_list = [fits.getdata(os.path.join(folder_path, file), ext=0) for file in files]
@@ -190,3 +196,81 @@ def sigma_clip(df, cols, sigma):
         std = df[col].std()
         df = df[(df[col] >= mean - sigma * std) & (df[col] <= mean + sigma * std)]
     return df
+
+########################
+
+def calc_fits_wv_1d(hdr, key_a='CRVAL1', key_b='CDELT1', key_c='NAXIS1'):
+    '''
+    Compute wavelength axis from keywords on spectrum header.
+    '''
+    return hdr[key_a] + hdr[key_b] * np.arange(hdr[key_c])
+
+########################
+
+def correct_spec_rv(wv, rv, units):
+    '''
+    Correct wavelength of spectrum by the RV of the star with a Doppler shift.
+    '''
+    c = 299792.458 #km/s
+    if units == "m/s":
+        c *= 1000
+    #delta_wv = wv * rv / c #shift
+    #wv_corr = wv + delta_wv #o problema era o sinal... é wv - delta_wv
+    wv_corr = wv / (1+rv/c)
+    delta_wv = wv - wv_corr
+    return wv_corr, np.mean(delta_wv)
+
+########################
+
+def read_fits(file_name):
+    '''
+    Read fits file and get header and data.
+    '''
+    hdul = fits.open(file_name)
+    spectrum = hdul[0].data
+    header = hdul[0].header
+    hdul.close()
+    return spectrum, header
+
+########################
+
+def get_rv_ccf(star, spectrum_filename, template_hdr, template_spec, drv, units):
+    '''
+    Uses crosscorrRV function from PyAstronomy to get the CCF of the star comparing with a spectrum of the Sun.
+    Returns the BJD in days, the RV, the max value of the CCF, the list of possible RV and CC, as well as the flux and raw wavelength.
+    To maximize the search for RV and avoid errors, the script searches the RV in SIMBAD and makes a reasonable interval. 
+    '''
+    stellar_spectrum, stellar_header = read_fits(file_name=spectrum_filename)
+    bjd = stellar_header["HIERARCH ESO DRS BJD"] #may change with instrument
+
+    #get wavelength and flux of both sun (template) and star
+    w = calc_fits_wv_1d(stellar_header); f = stellar_spectrum
+    tw = calc_fits_wv_1d(template_hdr); tf = template_spec
+    
+    #tw_condition = np.logical_and(tw > 6000, tw < 6200)
+    #w_condition = np.logical_and(w > 6000, w < 6200)
+
+    #tw_new = tw[tw_condition]
+    #tf_new = tf[tw_condition]
+    #w_new = w[w_condition]
+    #f_new = f[w_condition]
+
+    try:
+        rv_simbad = _get_simbad_data(star=star, alerts=False)["RV_VALUE"]
+        rvmin = rv_simbad - 1; rvmax = rv_simbad + 1
+    except:
+        rvmin = -200; rvmax = 200
+
+    #get the cross-correlation
+    rv, cc = pyasl.crosscorrRV(w = w, f = f, tw = tw, 
+                               tf = tf, rvmin = rvmin, rvmax = rvmax, drv = drv, skipedge = 200)
+
+    #index of the maximum cross-correlation function
+    maxind = np.argmax(cc)
+    radial_velocity = rv[maxind]
+
+    if units == "m/s":
+        radial_velocity *= 1000
+        rv *= 1000
+    
+    return bjd, radial_velocity, cc[maxind], rv, cc, w, f
