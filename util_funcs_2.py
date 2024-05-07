16d27607cfd1838d365510aa1651b97e2ddd8c73
telmonteiro <telmo.monteiro@ua.pt>
2024-05-07 10:29:50 +0100

    Update util_funcs_2.py

--- util_funcs_2.py
+++ util_funcs_2.py
@@ -277,7 +277,7 @@ def plot_line(data, line, line_legend="", lstyle = "-", legend_plot = False, plo
     line_wv = lines_list[line]
     
     if line in ["CaIIK","CaIIH"]: window = 12
-    elif line in ["Ha","NaID1","NaID2"]: window = 20
+    elif line in ["Ha","NaID1","NaID2"]: window = 12
     else: window = 0.7
 
     for array in data:
